c276a327fedc417495c9d18ef50dbc41dae9edfb
jlanej <jlanej@gmail.com>
2017-12-08 09:11:26 -0600

    log loop for scale

--- explore/openCyto/LOD/SubSample.R
+++ explore/openCyto/LOD/SubSample.R
@@ -1,22 +1,23 @@
 
 
 
-
-subSample <- function(fcsFile,outputDir){
+fcsFile = "/Volumes/Beta/data/flow/fcs3/2017-04-10_PANEL 2_FORTESSA_DHS_group one_F1638751_008.fcs"
+outputDir = "/Volumes/Beta/data/flow/isLOD/"
+events = c(2:10 %o% 10 ^ (3:5))
+subSample <- function(fcsFile, outputDir, events) {
   require(flowCore)
   
-   baseOut = paste0(outputDir, basename(gsub(".fcs","",fixed = TRUE,fcsFile)))
+  baseOut = paste0(outputDir, basename(gsub(".fcs", "", fixed = TRUE, fcsFile)))
   frame = read.FCS(fcsFile)
   
-  
-  inFile <- system.file("extdata", "0877408774.B08", package="flowCore")
-  foo <- read.FCS(inFile, transform=FALSE)
-  outFile <- file.path(tempdir(), "foo.fcs")
-  
-  ## now write out into a file
-  write.FCS(foo, outFile)
-  bar <- read.FCS(outFile, transform=FALSE)
-  all(exprs(foo) == exprs(bar))
+  for (eventCount in events) {
+    subOut=paste0(baseOut,"_",eventCount,".fcs")
+    write.FCS(frame, subOut)
+    
+    # ## now write out into a file
+    # bar <- read.FCS(outFile, transform = FALSE)
+    # all(exprs(foo) == exprs(bar))
+  }
   
   
 }
