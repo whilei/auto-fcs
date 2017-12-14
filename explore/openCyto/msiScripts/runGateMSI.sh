9da0522c503cb50c2a88f2bae4b04b3762779fdc
jlanej <jlanej@gmail.com>
2017-12-14 16:06:15 -0600

    new LOD

--- explore/openCyto/msiScripts/runGateMSI.sh
+++ explore/openCyto/msiScripts/runGateMSI.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
 module load java
-rev=r25LODTest_12full
+rev=r25LODTest_13full
 OUTDIR="/scratch.global/lanej/flow/full/results_"$rev"/"
 REPO_DIR="/home/tsaim/lane0212/git/auto-fcs/"
 p1Full=$REPO_DIR/explore/openCyto/lymph.dev.LSR.f.txt
