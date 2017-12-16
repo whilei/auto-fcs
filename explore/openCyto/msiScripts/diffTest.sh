6eb24ce4027ad7d7a969963b3b1276a86489fd46
jlanej <jlanej@gmail.com>
2017-12-16 08:16:19 -0600

    :floppy_disk:

--- explore/openCyto/msiScripts/diffTest.sh
+++ explore/openCyto/msiScripts/diffTest.sh
@@ -6,3 +6,6 @@ one_F1636108_016.fcs_panel1.wsp
 dir1=/scratch.global/lanej/flow/full/results_r25_9full/FULL/openCytoBatch_0/gatesRename/
 dir2=/scratch.global/lanej/flow/full/results_r25_15full/FULL/openCytoBatch_0/gatesRename/
 diff $dir1"2016-12-09_PANEL 1_HB_group one_F1636108_016.fcs_panel1Rename.wsp" $dir2"2016-12-09_PANEL 1_HB_group one_F1636108_016.fcs_panel1Rename.wsp"
+
+
+grep Error *.o
