b8e41973f7dc8f6287d321c0fe8885a9ebef4d9a
jlanej <jlanej@gmail.com>
2017-12-18 15:29:25 -0600

    :floppy_disk:

--- explore/openCyto/msiScripts/seedDL.sh
+++ explore/openCyto/msiScripts/seedDL.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 F1631217_003
-
+F1638813_029
 
 mkdir rand
 scp -r msi:/home/thyagara/shared/HRS/annotation/r25/panel1/*F1631217_003* ./rand
 scp -r msi:/home/thyagara/shared/HRS/annotation/r25/panel1/*F1631927_005* ./rand
+scp -r msi:/home/thyagara/shared/HRS/annotation/r25/panel1/*F1638813_029* ./rand
 
 mkdir 42
 
@@ -15,10 +16,13 @@ scp -r msi:/scratch.global/lanej/flow/full/results_r25_17full/FULL/*/panel1Vis/*
 
 
 
-filePat=F1631376_005
+filePat=F1638813_029
 scp -r msi:/home/thyagara/shared/HRS/annotation/r25/panel1/*$filePat* ./rand
 scp -r msi:/scratch.global/lanej/flow/full/results_r25_17full/FULL/*/panel1Vis/*$filePat* ./42
 
 mkdir -p $filePat/comp
+
+convert -delay 100 -loop 0 *.png F1638813_029.gif
+
 
 cp 
