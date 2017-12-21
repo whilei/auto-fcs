2524d9959b0de0a02d7f4608ea5318fc2927a521
jlanej <jlanej@gmail.com>
2017-12-21 12:27:08 -0600

    updated special singlet files

---
+++ explore/openCyto/MissFcs.md
@@ -0,0 +1,13 @@
+Exclude files
+
+
+2016-11-04    Ctl-D_ZF_004
+2016-11-04    Ctl-D_HB_008
+2016-11-04    Ctl-D_ZF_004
+2016-11-04    Ctl-D_HB_008
+2017-04-19    ZF-WB_24HRS_009
+2017-04-19    BC-WB_24HRS_007
+2017-04-19    RR-WB_24HRS_008
+2017-05-17    ZF-postUVlaseralignement_speedadjust_group_two_ZF_F1632476_020
+
+
--- explore/openCyto/msiScripts/ManualGrep.sh
+++ explore/openCyto/msiScripts/ManualGrep.sh
@@ -4,5 +4,7 @@
 
 for i in $(cat toTrans.txt); do
    echo $i
-   cp -r ./*/panel2Vis/*$i* /home/thyagara/shared/HRS/annotation/r25_SingleCellBranch/panel2/
+   cp -r ./*/panel2Vis/*$i* /home/thyagara/shared/HRS/annotation/r25_SingleCellBranch/panel2_onlyLiveSingleManuals/
 done
+
+grep -i "manual" * |grep "Live_Single_immune_cells"|cut -f 2 -d "|"| sed "s/.fcs.*//g"|sed "s/.*\///g"|sort |uniq |grep -v ANNOT >toTrans.txt
