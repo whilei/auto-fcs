6f771ae8b3963dde8e1d3964265713c882c7b176
jlanej <jlanej@gmail.com>
2018-01-05 12:45:08 -0600

    batch setup

---
+++ explore/openCyto/LOD/SubSample_NonCtls.R
@@ -0,0 +1,54 @@
+
+
+outputDir = "/scratch.global/lanej/flow/full/insilicoLOD_GoodMatched/"
+events = c(2:10 %o% 10 ^ (3:5))
+inputFile="/home/tsaim/lane0212/git/auto-fcs/explore/openCyto/LOD/"
+
+subSample <- function(fcsFile, outputDir, events, iter) {
+  require(flowCore)
+  
+  baseOut = paste0(outputDir, basename(gsub(".fcs", "", fixed = TRUE, fcsFile)))
+  frame = read.FCS(fcsFile)
+  maxEvents = length(exprs(frame)[, "FSC-H"])
+  
+  if (iter == 1) {
+    write.FCS(frame, paste0(baseOut, "_All_", maxEvents, ".fcs"))
+  }
+  
+  for (eventCount in events) {
+    if (maxEvents * .5 > eventCount) {
+      subOut = paste0(baseOut, "_count_", eventCount, "_iter_", iter, ".fcs")
+      if (!file.exists(subOut)) {
+        sampler = sample(1:maxEvents, eventCount, replace = FALSE)
+        frameSub = frame[sampler, ]
+        write.FCS(frameSub, subOut)
+      }
+    }
+  }
+}
+
+inputDir = "/scratch.global/lanej/flow/full/fcs/"
+
+fcsFilesAll <-
+  list.files(inputDir,
+             pattern = ".fcs",
+             full = TRUE)
+
+
+iters = c(1:10)
+
+fcsFilesRun = c(p1Files, p2Files)
+
+# fcsFilesRun = fcsFilesAll
+set.seed(42)
+for (fcsFile in fcsFilesRun) {
+  for (iter in iters) {
+    print(paste0("sampling file ", fcsFile, " iteration ", iter))
+    subSample(
+      fcsFile = fcsFile,
+      outputDir = outputDir,
+      events = events,
+      iter = iter
+    )
+  }
+}
