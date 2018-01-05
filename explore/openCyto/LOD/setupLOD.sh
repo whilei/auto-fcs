ac7d2f1619209058fa056e16ca2b0a76866c8a92
jlanej <jlanej@gmail.com>
2018-01-05 13:06:30 -0600

    fewer resources

--- explore/openCyto/LOD/setupLOD.sh
+++ explore/openCyto/LOD/setupLOD.sh
@@ -7,7 +7,8 @@ batch=12
 batchIters=$(($batch))
 for i in `seq 1 $batchIters`;do
 	grep -v "Rscript" /home/tsaim/lane0212/git/auto-fcs/explore/openCyto/msiScripts/InsilicoLOD.sh > $outDir/"InsilicoLOD_$i.qsub"
-	cp /Users/Kitty/git/auto-fcs/explore/openCyto/LOD/SubSample_NonCtls.R $outDir/"SubSample_NonCtls_$i.R"
+	echo "Rscript $outDir/SubSample_NonCtls_$i.R" >> $outDir/"InsilicoLOD_$i.qsub"
+	cp /home/tsaim/lane0212/git/auto-fcs/explore/openCyto/LOD/SubSample_NonCtls.R $outDir/"SubSample_NonCtls_$i.R"
 	batchFile="P1_P2_matched.goods.batch$i.txt"
 	sed -i "s/replaceWithBatch/$batchFile/g" $outDir/"SubSample_NonCtls_$i.R"
 done	
