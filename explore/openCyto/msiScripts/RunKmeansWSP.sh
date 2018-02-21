1f2ba55fe7757b547e59d471dc6485dafe0ed95c
jlanej <jlanej@gmail.com>
2018-02-20 18:01:07 -0600

    :floppy_disk:

--- explore/openCyto/msiScripts/RunKmeansWSP.sh
+++ explore/openCyto/msiScripts/RunKmeansWSP.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash 
 
-repoDir="/home/tsaim/lane0212/git/auto-fcs/"
-repoDir="/Users/Kitty/git/auto-fcs/explore/openCyto/"
-workspaceDir="/Volumes/Beta/data/flow/testTcellSubFCS_BoolResults/"
-fcsDir="/Volumes/Beta/data/flow/testTcellSubFCS/"
-outputDir="/Volumes/Beta/data/flow/testTcellSubWSP_Start/"
+
+module load R/3.3.3
+module load gcc/6.1.0
+module load hdf5/hdf5-1.8.9-intel
+module load libtiff
+
+# repoDir="/Users/Kitty/git/auto-fcs/explore/openCyto/"
+repoDir="/home/tsaim/lane0212/git/auto-fcs/explore/openCyto/"
+
+# workspaceDir="/Volumes/Beta/data/flow/testTcellSubFCS_BoolResults/"
+workspaceDir="/scratch.global/lanej/flow/manual/allManuals/"
+
+# fcsDir="/Volumes/Beta/data/flow/testTcellSubFCS/"
+fcsDir="/Volumes/Beta/data/flow/full/fcs/"
+
+# outputDir="/Volumes/Beta/data/flow/testTcellSubWSP_Start/"
+outputDir="/scratch.global/lanej/flow/manual/kmeans/"
 
 Rscript $repoDir"KmeansWSP.R" --workspaceDir $workspaceDir --fcsDir $fcsDir --outputDir $outputDir --repoDir $repoDir
 
---
+++ explore/openCyto/revise/priority.md
@@ -0,0 +1,3 @@
+1. Original manual gates
+2. Full manual gates
+3. Bcell specific gates
