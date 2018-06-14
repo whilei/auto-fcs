f32932693f72e0abe1c9014cb229e6f6a8e4e205
jlanej <jlanej@gmail.com>
2018-06-14 13:03:02 -0500

    start of control comparision

---
+++ explore/novel/report/MetClustNewNormV1.md
@@ -0,0 +1,20 @@
+---
+title: "MetaClusters"
+author: "JL"
+date: "6/14/2018"
+output: 
+  html_document: 
+    keep_md: yes
+---
+
+
+
+## Tests
+
+
+![](./map.png)
+
+
+
+![](MetClustNewNormV1_files/figure-html/unnamed-chunk-1-1.png)<!-- -->![](MetClustNewNormV1_files/figure-html/unnamed-chunk-1-2.png)<!-- -->
+
--- explore/novel/scripts/collectCents.sh
+++ explore/novel/scripts/collectCents.sh
@@ -8,3 +8,13 @@ mkdir -p $outDir
 rsync -avz msi:$msiDir/*cents.scale $outDir
 
 rsync -avz msi:$msiDir/*cents.inputData $outDir
+
+
+msiDir=/scratch.global/lanej/flow/novel/detect_v1/
+outDir=/Volumes/Beta2/flow/detect_v1/
+
+mkdir -p $outDir
+
+rsync -avz msi:$msiDir/*cents.scale $outDir
+
+rsync -avz msi:$msiDir/*cents.inputData $outDir
