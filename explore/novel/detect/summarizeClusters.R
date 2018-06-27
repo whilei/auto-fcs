d4cd82c99c556c94dfd5ebabe85a216495465a16
jlanej <jlanej@gmail.com>
2018-06-27 17:21:58 -0500

    should be simple

--- explore/novel/detect/summarizeClusters.R
+++ explore/novel/detect/summarizeClusters.R
@@ -6,32 +6,38 @@ summarize <-
     combo = cbind(phenoGraphClusters, knownPopulations)
     combo = combo[which(combo[, phenoColumn] >= 0), ]
     phenoClusts = sort(unique(combo[, phenoColumn]))
+    
+    combo$POP_NAMES_SUB=NA
+    combo[which(combo$CYTO_T&combo$effector.memory),]$POP_NAMES_SUB="effector memory"
+    combo[which(combo$CYTO_T&combo$naive),]$POP_NAMES_SUB="naive"
+    combo[which(combo$CYTO_T&combo$central.memory),]$POP_NAMES_SUB="central memory"
+    combo[which(combo$CYTO_T&combo$effector),]$POP_NAMES_SUB="effector"
+    
+    
+    combo$POP_NAMES_SUB_SUB=NA
+    combo[which(combo$CYTO_T&combo$effector&combo$CD28M_CD27M),]$POP_NAMES_SUB_SUB="E"
+    combo[which(combo$CYTO_T&combo$effector&combo$CD28M_CD27P),]$POP_NAMES_SUB_SUB="pE2"
+    combo[which(combo$CYTO_T&combo$effector&combo$CD28P_CD27P),]$POP_NAMES_SUB_SUB="pE1"
+    combo[which(combo$CYTO_T&combo$effector&combo$CD28P_CD27M),]$POP_NAMES_SUB_SUB="CD28P_27M"
+    
+    
+    combo[which(combo$CYTO_T&combo$effector.memory&combo$CD28M_CD27M),]$POP_NAMES_SUB_SUB="EM3"
+    combo[which(combo$CYTO_T&combo$effector.memory&combo$CD28M_CD27P),]$POP_NAMES_SUB_SUB="EM2"
+    combo[which(combo$CYTO_T&combo$effector.memory&combo$CD28P_CD27P),]$POP_NAMES_SUB_SUB="EM1"
+    combo[which(combo$CYTO_T&combo$effector.memory&combo$CD28P_CD27M),]$POP_NAMES_SUB_SUB="EM4"
+    combo=combo[which(!is.na(combo$POP_NAMES_SUB)),]
+    
+    
+    subInterest=c("POP_NAMES_SUB","POP_NAMES_SUB_SUB")
+    combo=combo[,c(phenoColumn,subInterest)]
     summary = as.data.frame(table(combo))
     
-    for(known in knownColumns){
-      summary[,known]=as.logical(summary[,known])
+    for(known in subInterest){
+      summary[,known]=as.character(summary[,known])
     }
     
 
-    summary$POP_NAMES_SUB=NA
-    summary[which(summary$CYTO_T&summary$effector.memory),]$POP_NAMES_SUB="effector memory"
-    summary[which(summary$CYTO_T&summary$naive),]$POP_NAMES_SUB="naive"
-    summary[which(summary$CYTO_T&summary$central.memory),]$POP_NAMES_SUB="central memory"
-    summary[which(summary$CYTO_T&summary$effector),]$POP_NAMES_SUB="effector"
     
-    
-    summary$POP_NAMES_SUB_SUB=NA
-    summary[which(summary$CYTO_T&summary$effector&summary$CD28M_CD27M),]$POP_NAMES_SUB_SUB="E"
-    summary[which(summary$CYTO_T&summary$effector&summary$CD28M_CD27P),]$POP_NAMES_SUB_SUB="pE2"
-    summary[which(summary$CYTO_T&summary$effector&summary$CD28P_CD27P),]$POP_NAMES_SUB_SUB="pE1"
-    summary[which(summary$CYTO_T&summary$effector&summary$CD28P_CD27M),]$POP_NAMES_SUB_SUB="CD28P_27M"
-    
-    
-    summary[which(summary$CYTO_T&summary$effector.memory&summary$CD28M_CD27M),]$POP_NAMES_SUB_SUB="EM3"
-    summary[which(summary$CYTO_T&summary$effector.memory&summary$CD28M_CD27P),]$POP_NAMES_SUB_SUB="EM2"
-    summary[which(summary$CYTO_T&summary$effector.memory&summary$CD28P_CD27P),]$POP_NAMES_SUB_SUB="EM1"
-    summary[which(summary$CYTO_T&summary$effector.memory&summary$CD28P_CD27M),]$POP_NAMES_SUB_SUB="EM4"
-    summary=summary[which(!is.na(summary$POP_NAMES_SUB)),]
     
     
     totalPhenograph=as.data.frame(table(combo[,phenoColumn]))
@@ -41,9 +47,18 @@ summarize <-
     summary$cytotoxic.Tcells.CD8.=as.character(summary$cytotoxic.Tcells.CD8.)
     
     summary=merge(summary,totalPhenograph,by.y="PHENOGRAPH_CLUSTER",by.x="cytotoxic.Tcells.CD8.")
+    summary$FREQ_TOTAL=summary$Freq/summary$TOTAL_PHENOGRAPH_COUNTS
+    
+    freqSub=sort(unique(summary$POP_NAMES_SUB))
+    freqSubSub=sort(unique(summary$POP_NAMES_SUB_SUB))
+    
+    summaryFreq= data.frame(PHENOGRAPH_CLUSTER=totalPhenograph$PHENOGRAPH_CLUSTER,TOTAL_PHENOGRAPH_COUNTS=totalPhenograph$TOTAL_PHENOGRAPH_COUNTS)
     
     
-    
+    for(sub in freqSub){
+      tmp=summary[which(summary$POP_NAMES_SUB==sub),]
+      print(length(tmp$POP_NAMES_SUB))
+    }
     
   }
 
