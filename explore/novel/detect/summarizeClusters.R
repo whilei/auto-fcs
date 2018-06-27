d15d54e290b7d5bb1f5ef5797734ee4ec69ea46b
jlanej <jlanej@gmail.com>
2018-06-27 16:47:19 -0500

    pop summary

--- explore/novel/detect/summarizeClusters.R
+++ explore/novel/detect/summarizeClusters.R
@@ -1,15 +1,4 @@
 
-
-
-
-
-
-
-
-
-
-
-
 summarize <-
   function(phenoGraphClusters, knownPopulations) {
     phenoColumn = colnames(phenoGraphClusters)
@@ -23,12 +12,37 @@ summarize <-
       summary[,known]=as.logical(summary[,known])
     }
     
-    
-    
+
     summary$POP_NAMES_SUB=NA
+    summary[which(summary$CYTO_T&summary$effector.memory),]$POP_NAMES_SUB="effector memory"
+    summary[which(summary$CYTO_T&summary$naive),]$POP_NAMES_SUB="naive"
+    summary[which(summary$CYTO_T&summary$central.memory),]$POP_NAMES_SUB="central memory"
+    summary[which(summary$CYTO_T&summary$effector),]$POP_NAMES_SUB="effector"
+    
+    
+    summary$POP_NAMES_SUB_SUB=NA
+    summary[which(summary$CYTO_T&summary$effector&summary$CD28M_CD27M),]$POP_NAMES_SUB_SUB="E"
+    summary[which(summary$CYTO_T&summary$effector&summary$CD28M_CD27P),]$POP_NAMES_SUB_SUB="pE2"
+    summary[which(summary$CYTO_T&summary$effector&summary$CD28P_CD27P),]$POP_NAMES_SUB_SUB="pE1"
+    summary[which(summary$CYTO_T&summary$effector&summary$CD28P_CD27M),]$POP_NAMES_SUB_SUB="CD28P_27M"
+    
+    
+    summary[which(summary$CYTO_T&summary$effector.memory&summary$CD28M_CD27M),]$POP_NAMES_SUB_SUB="EM3"
+    summary[which(summary$CYTO_T&summary$effector.memory&summary$CD28M_CD27P),]$POP_NAMES_SUB_SUB="EM2"
+    summary[which(summary$CYTO_T&summary$effector.memory&summary$CD28P_CD27P),]$POP_NAMES_SUB_SUB="EM1"
+    summary[which(summary$CYTO_T&summary$effector.memory&summary$CD28P_CD27M),]$POP_NAMES_SUB_SUB="EM4"
+    summary=summary[which(!is.na(summary$POP_NAMES_SUB)),]
+    
     
     totalPhenograph=as.data.frame(table(combo[,phenoColumn]))
-
+    totalPhenograph$PHENOGRAPH_CLUSTER=as.character(totalPhenograph$Var1)
+    totalPhenograph$TOTAL_PHENOGRAPH_COUNTS=totalPhenograph$Freq
+    totalPhenograph=totalPhenograph[,c("PHENOGRAPH_CLUSTER","TOTAL_PHENOGRAPH_COUNTS")]
+    summary$cytotoxic.Tcells.CD8.=as.character(summary$cytotoxic.Tcells.CD8.)
+    
+    summary=merge(summary,totalPhenograph,by.y="PHENOGRAPH_CLUSTER",by.x="cytotoxic.Tcells.CD8.")
+    
+    
     
     
   }
