134e98f1152eb999f7c27a43e5812c40f52d021a
jlanej <jlanej@gmail.com>
2018-06-27 17:42:43 -0500

    pop summary

--- explore/novel/detect/summarizeClusters.R
+++ explore/novel/detect/summarizeClusters.R
@@ -1,64 +1,74 @@
 
+
+
+
 summarize <-
   function(phenoGraphClusters, knownPopulations) {
     phenoColumn = colnames(phenoGraphClusters)
     knownColumns = colnames(knownPopulations)
     combo = cbind(phenoGraphClusters, knownPopulations)
-    combo = combo[which(combo[, phenoColumn] >= 0), ]
+    combo = combo[which(combo[, phenoColumn] >= 0),]
     phenoClusts = sort(unique(combo[, phenoColumn]))
     
-    combo$POP_NAMES_SUB=NA
-    combo[which(combo$CYTO_T&combo$effector.memory),]$POP_NAMES_SUB="effector memory"
-    combo[which(combo$CYTO_T&combo$naive),]$POP_NAMES_SUB="naive"
-    combo[which(combo$CYTO_T&combo$central.memory),]$POP_NAMES_SUB="central memory"
-    combo[which(combo$CYTO_T&combo$effector),]$POP_NAMES_SUB="effector"
+    combo$POP_NAMES_SUB = NA
+    combo[which(combo$CYTO_T &
+                  combo$effector.memory), ]$POP_NAMES_SUB = "effector memory"
+    combo[which(combo$CYTO_T &
+                  combo$naive), ]$POP_NAMES_SUB = "naive"
+    combo[which(combo$CYTO_T &
+                  combo$central.memory), ]$POP_NAMES_SUB = "central memory"
+    combo[which(combo$CYTO_T &
+                  combo$effector), ]$POP_NAMES_SUB = "effector"
     
     
-    combo$POP_NAMES_SUB_SUB=NA
-    combo[which(combo$CYTO_T&combo$effector&combo$CD28M_CD27M),]$POP_NAMES_SUB_SUB="E"
-    combo[which(combo$CYTO_T&combo$effector&combo$CD28M_CD27P),]$POP_NAMES_SUB_SUB="pE2"
-    combo[which(combo$CYTO_T&combo$effector&combo$CD28P_CD27P),]$POP_NAMES_SUB_SUB="pE1"
-    combo[which(combo$CYTO_T&combo$effector&combo$CD28P_CD27M),]$POP_NAMES_SUB_SUB="CD28P_27M"
+    combo$POP_NAMES_SUB_SUB = NA
+    combo[which(combo$CYTO_T &
+                  combo$effector &
+                  combo$CD28M_CD27M), ]$POP_NAMES_SUB_SUB = "E"
+    combo[which(combo$CYTO_T &
+                  combo$effector &
+                  combo$CD28M_CD27P), ]$POP_NAMES_SUB_SUB = "pE2"
+    combo[which(combo$CYTO_T &
+                  combo$effector &
+                  combo$CD28P_CD27P), ]$POP_NAMES_SUB_SUB = "pE1"
+    combo[which(combo$CYTO_T &
+                  combo$effector &
+                  combo$CD28P_CD27M), ]$POP_NAMES_SUB_SUB = "CD28P_27M"
     
     
-    combo[which(combo$CYTO_T&combo$effector.memory&combo$CD28M_CD27M),]$POP_NAMES_SUB_SUB="EM3"
-    combo[which(combo$CYTO_T&combo$effector.memory&combo$CD28M_CD27P),]$POP_NAMES_SUB_SUB="EM2"
-    combo[which(combo$CYTO_T&combo$effector.memory&combo$CD28P_CD27P),]$POP_NAMES_SUB_SUB="EM1"
-    combo[which(combo$CYTO_T&combo$effector.memory&combo$CD28P_CD27M),]$POP_NAMES_SUB_SUB="EM4"
-    combo=combo[which(!is.na(combo$POP_NAMES_SUB)),]
+    combo[which(combo$CYTO_T &
+                  combo$effector.memory &
+                  combo$CD28M_CD27M), ]$POP_NAMES_SUB_SUB = "EM3"
+    combo[which(combo$CYTO_T &
+                  combo$effector.memory &
+                  combo$CD28M_CD27P), ]$POP_NAMES_SUB_SUB = "EM2"
+    combo[which(combo$CYTO_T &
+                  combo$effector.memory &
+                  combo$CD28P_CD27P), ]$POP_NAMES_SUB_SUB = "EM1"
+    combo[which(combo$CYTO_T &
+                  combo$effector.memory &
+                  combo$CD28P_CD27M), ]$POP_NAMES_SUB_SUB = "EM4"
+    combo = combo[which(!is.na(combo$POP_NAMES_SUB)), ]
     
     
-    subInterest=c("POP_NAMES_SUB","POP_NAMES_SUB_SUB")
-    combo=combo[,c(phenoColumn,subInterest)]
-    summary = as.data.frame(table(combo))
-    
-    for(known in subInterest){
-      summary[,known]=as.character(summary[,known])
-    }
-    
-
+    subInterest = c("POP_NAMES_SUB", "POP_NAMES_SUB_SUB")
+    combo = combo[, c(phenoColumn, subInterest)]
+    summarySUB = as.data.frame.matrix(table(combo$cytotoxic.Tcells.CD8., combo$POP_NAMES_SUB))
+    summarySUB_SUB = as.data.frame.matrix(table(combo$cytotoxic.Tcells.CD8., combo$POP_NAMES_SUB_SUB))
+    summary = cbind(summarySUB, summarySUB_SUB)
+    summary$PHENOGRAPH_CLUSTER = row.names(summary)
     
     
     
-    totalPhenograph=as.data.frame(table(combo[,phenoColumn]))
-    totalPhenograph$PHENOGRAPH_CLUSTER=as.character(totalPhenograph$Var1)
-    totalPhenograph$TOTAL_PHENOGRAPH_COUNTS=totalPhenograph$Freq
-    totalPhenograph=totalPhenograph[,c("PHENOGRAPH_CLUSTER","TOTAL_PHENOGRAPH_COUNTS")]
-    summary$cytotoxic.Tcells.CD8.=as.character(summary$cytotoxic.Tcells.CD8.)
+    totalPhenograph = as.data.frame(table(combo[, phenoColumn]))
+    totalPhenograph$PHENOGRAPH_CLUSTER = as.character(totalPhenograph$Var1)
+    totalPhenograph$TOTAL_PHENOGRAPH_COUNTS = totalPhenograph$Freq
+    totalPhenograph = totalPhenograph[, c("PHENOGRAPH_CLUSTER", "TOTAL_PHENOGRAPH_COUNTS")]
     
-    summary=merge(summary,totalPhenograph,by.y="PHENOGRAPH_CLUSTER",by.x="cytotoxic.Tcells.CD8.")
-    summary$FREQ_TOTAL=summary$Freq/summary$TOTAL_PHENOGRAPH_COUNTS
+    summary = merge(summary, totalPhenograph, by.y = "PHENOGRAPH_CLUSTER", by.x =
+                      "PHENOGRAPH_CLUSTER")
     
-    freqSub=sort(unique(summary$POP_NAMES_SUB))
-    freqSubSub=sort(unique(summary$POP_NAMES_SUB_SUB))
-    
-    summaryFreq= data.frame(PHENOGRAPH_CLUSTER=totalPhenograph$PHENOGRAPH_CLUSTER,TOTAL_PHENOGRAPH_COUNTS=totalPhenograph$TOTAL_PHENOGRAPH_COUNTS)
-    
-    
-    for(sub in freqSub){
-      tmp=summary[which(summary$POP_NAMES_SUB==sub),]
-      print(length(tmp$POP_NAMES_SUB))
-    }
+    return(summary)
     
   }
 
@@ -79,8 +89,17 @@ for (file in intclusts) {
                                     stringsAsFactors = FALSE,
                                     header = TRUE)
     
-    summarize(phenoGraphClusters = phenoGraphClusters,
-              knownPopulations = knownPopulations)
+    summary = summarize(phenoGraphClusters = phenoGraphClusters,
+                        knownPopulations = knownPopulations)
+    write.table(
+      summary,
+      file = gsub(".IntMatrix.txt.gz",
+                  ".known.pops.txt", file),
+      row.names = FALSE,
+      quote = FALSE,
+      sep = "\t",
+      col.names = TRUE
+    )
   }
   
 }
