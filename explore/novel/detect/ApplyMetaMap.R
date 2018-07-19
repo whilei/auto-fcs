d417b265c170b6e603d2e68880bc0129499abc7d
jlanej <jlanej@gmail.com>
2018-07-19 09:17:31 -0500

    setup for meta-map application

---
+++ explore/novel/detect/ApplyMetaMap.R
@@ -0,0 +1,26 @@
+
+
+library(optparse)
+option_list = list(
+  make_option(
+    c("-map", "--inputMap"),
+    type = "character",
+    help = "map file used to re-assign clusters",
+    metavar = "character",
+    default = "/Users/Kitty/git/auto-fcs/explore/novel/report/summary.metaMap"
+  ),
+  make_option(
+    c("-o", "--outputDir"),
+    type = "character",
+    help = "output directory",
+    metavar = "character",
+    default = "/Volumes/Beta2/flow/testSummaryMetaMap/"
+  )
+)
+
+
+opt_parser = OptionParser(option_list = option_list)
+
+opt = parse_args(opt_parser)
+outDir = opt$outputDir
+dir.create(outDir)
