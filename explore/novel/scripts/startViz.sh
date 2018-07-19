bc0fd9276d7be58bfc4c7e70ff42cd63b3dfd875
jlanej <jlanej@gmail.com>
2018-07-19 17:20:48 -0500

    flow Anno script

--- explore/novel/scripts/startViz.sh
+++ explore/novel/scripts/startViz.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash 
+
+
+cd /Volumes/Beta2/flow
+
+
+dir=detect_NoNorm_v5_control_examples_meta_viz_2
+mkdir -p $dir
+
+cd $dir
+
+
+rsync -avz msi:/scratch.global/lanej/flow/novel/$dir/* ./
+
+java -jar /Users/Kitty/workspace.other/Genvisis/Assembly/target/genvisis.jar org.genvisis.one.ben.flowannot.FlowAnnotator
