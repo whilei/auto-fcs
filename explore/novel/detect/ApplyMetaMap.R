ab9e04e06902e7710c68dd37d04f1616bf08a57b
jlanej <jlanej@gmail.com>
2018-07-19 15:19:35 -0500

    apply sample metas

--- explore/novel/detect/ApplyMetaMap.R
+++ explore/novel/detect/ApplyMetaMap.R
@@ -2,6 +2,10 @@
 
 
 
+
+
+
+
 library(optparse)
 option_list = list(
   make_option(
@@ -33,3 +37,51 @@ opt_parser = OptionParser(option_list = option_list)
 opt = parse_args(opt_parser)
 outDir = opt$outputDir
 dir.create(outDir)
+
+
+intMatFiles = list.files(opt$inputDirectory,
+                         full.names = TRUE,
+                         pattern = ".IntMatrix.txt.gz$")
+
+metaMap = read.delim(opt$inputMap ,
+                     stringsAsFactors = FALSE,
+                     header = TRUE)
+
+
+for (matFile in intMatFiles) {
+  print(matFile)
+  sample = basename(matFile)
+  sample = gsub("_subFirst_TRUE_normalize_FALSE.IntMatrix.txt.gz",
+                "",
+                sample)
+  sampleMeta = metaMap[which(metaMap$SAMPLE == sample), ]
+  
+  print(length(sampleMeta$SAMPLE))
+  mat = read.delim(matFile ,
+                   stringsAsFactors = FALSE,
+                   header = TRUE)
+  newMat = data.frame(DEF = mat$cytotoxic.Tcells.CD8.)
+  newMat$META_CLUSTER = -1
+  for (pOrig in unique(sampleMeta$PHENOGRAPH_CLUSTER)) {
+    sub = newMat$DEF == pOrig
+    new = sampleMeta[which(sampleMeta$PHENOGRAPH_CLUSTER == pOrig), ]$META_CLUSTER
+    newMat$META_CLUSTER[sub] = new
+  }
+  
+  newMat = as.data.frame(newMat[, c("META_CLUSTER")])
+  colnames(newMat) = "cytotoxic Tcells-CD8+"
+  print(length(newMat$`cytotoxic Tcells-CD8+`))
+  print(length(mat$cytotoxic.Tcells.CD8.))
+  t = table(mat$cytotoxic.Tcells.CD8., newMat$`cytotoxic Tcells-CD8+`)
+  print(paste0("-1 sums of ", sum(t[1, ]), " and ", sum(t[, 1])))
+  gz1 <- gzfile(paste0(outDir, basename(matFile)), "w")
+  write.table(
+    newMat,
+    file = gz1 ,
+    sep = "\t",
+    quote = FALSE,
+    row.names = FALSE
+  )
+  close(gz1)
+  
+}
---
+++ explore/novel/scripts/runViz_meta.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#$ -cwd
+#$ -S /bin/bash
+#PBS -e $PBS_JOBNAME.$PBS_JOBID.e
+#PBS -o $PBS_JOBNAME.$PBS_JOBID.o
+#PBS -m ae
+#PBS -l mem=150gb,walltime=96:00:00,nodes=1:ppn=8
+
+module load java
+module load parallel
+module load R/3.5.0
+module load hdf5/hdf5-1.8.9-intel
+module load libtiff
+module load gcc/8.1.0
+
+
+
+
+
+
+
+genvisis=/home/tsaim/lane0212/genvisis.viz.jar
+baseDir=/scratch.global/lanej/flow/novel/detect_NoNorm_v5_control_examples/
+fcs=$baseDir
+wsp=$baseDir
+out=/scratch.global/lanej/flow/novel/detect_NoNorm_v5_control_examples_meta_viz/
+gateOverride=$baseDir
+repoDir=/home/tsaim/lane0212/git/auto-fcs
+gateOverrideMatchup=$repoDir/explore/novel/scripts/resources/ovvrMatch_update.txt
+clusterDir=$baseDir
+
+
+
+
+echo "prepping for meta clusters"
+mkdir -p $baseDir"bakInts/"
+
+mv $baseDir/*.IntMatrix.txt.gz  $baseDir"bakInts/"
+
+
+java -jar $genvisis org.genvisis.one.ben.fcs.auto.FCSProcessingPipeline \
+    fcs=$fcs \
+    wsp=$wsp \
+    out=$out \
+    panel=1 \
+    pipe=VIZ \
+    gateOverride=$gateOverride \
+    gateOverrideMatchup=$gateOverrideMatchup \
+    clusterDir=$clusterDir
