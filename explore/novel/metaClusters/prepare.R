72e93b87b7c9d080ca827c414dfbff2f3b126115
jlanej <jlanej@gmail.com>
2018-07-24 12:47:55 -0500

    :floppy_disk:

--- explore/novel/metaClusters/prepare.R
+++ explore/novel/metaClusters/prepare.R
@@ -1,4 +1,6 @@
 library(devtools)
 
-load("/Users/Kitty/git/auto-fcs/explore/novel/summary.meta.RData")
+load("/Users/Kitty/git/auto-fcs/explore/novel/report/summary.meta.RData")
 saveRDS(summary,file="/Users/Kitty/git/auto-fcs/explore/novel/metaClusters/data/summary.rds")
+
+print("prepared data")
