9c8eac26a2ea13bf5652fabaca2e270b497078ca
jlanej <jlanej@gmail.com>
2018-10-31 09:58:08 -0500

    start incorporating full OC populations

--- explore/novel/scripts/runSummarizeLocal.sh
+++ explore/novel/scripts/runSummarizeLocal.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-Rscript /Users/Kitty/git/auto-fcs/explore/novel/detect/summarizeClusters.R --inputDirectory "/Volumes/Beta2/flow/testSync/" --outputDir "/Volumes/Beta2/flow/testSummary/"
+Rscript /Users/Kitty/git/auto-fcs/explore/novel/detect/summarizeClusters.R --inputDirectory "/Volumes/Beta2/flow/testSyncLymph/" --outputDir "/Volumes/Beta2/flow/testSummaryLymph/"
