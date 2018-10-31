980e03e9ca215371515f8e3a9ac546f20db9edda
jlanej <jlanej@gmail.com>
2018-10-31 10:22:57 -0500

    :floppy_disk:

--- explore/novel/detect/summarizeClusters.R
+++ explore/novel/detect/summarizeClusters.R
@@ -9,6 +9,7 @@
 
 
 
+
 library(optparse)
 option_list = list(
   make_option(
@@ -38,32 +39,32 @@ summarize <-
     phenoColumn = colnames(phenoGraphClusters)
     knownColumns = colnames(knownPopulations)
     combo = cbind(phenoGraphClusters, knownPopulations)
-    combo = combo[which(combo[, phenoColumn] >= 0), ]
+    combo = combo[which(combo[, phenoColumn] >= 0),]
     phenoClusts = sort(unique(combo[, phenoColumn]))
     
     combo$POP_NAMES_SUB = NA
     if (length(combo[which(combo$CYTO_T &
-                           combo$effector.memory),]$POP_NAMES_SUB) > 0) {
+                           combo$effector.memory), ]$POP_NAMES_SUB) > 0) {
       combo[which(combo$CYTO_T &
-                    combo$effector.memory),]$POP_NAMES_SUB = "effector memory"
+                    combo$effector.memory), ]$POP_NAMES_SUB = "effector memory"
     }
     
     if (length(combo[which(combo$CYTO_T &
-                           combo$naive),]$POP_NAMES_SUB)  > 0) {
+                           combo$naive), ]$POP_NAMES_SUB)  > 0) {
       combo[which(combo$CYTO_T &
-                    combo$naive),]$POP_NAMES_SUB = "naive"
+                    combo$naive), ]$POP_NAMES_SUB = "naive"
     }
     
     if (length(combo[which(combo$CYTO_T &
-                           combo$central.memory),]$POP_NAMES_SUB) > 0) {
+                           combo$central.memory), ]$POP_NAMES_SUB) > 0) {
       combo[which(combo$CYTO_T &
-                    combo$central.memory),]$POP_NAMES_SUB = "central memory"
+                    combo$central.memory), ]$POP_NAMES_SUB = "central memory"
     }
     
     if (length(combo[which(combo$CYTO_T &
-                           combo$effector),]$POP_NAMES_SUB)  > 0) {
+                           combo$effector), ]$POP_NAMES_SUB)  > 0) {
       combo[which(combo$CYTO_T &
-                    combo$effector),]$POP_NAMES_SUB = "effector"
+                    combo$effector), ]$POP_NAMES_SUB = "effector"
     }
     
     
@@ -71,72 +72,72 @@ summarize <-
     
     if (length(combo[which(combo$CYTO_T &
                            combo$effector &
-                           combo$CD28M_CD27M),]$POP_NAMES_SUB_SUB)  > 0) {
+                           combo$CD28M_CD27M), ]$POP_NAMES_SUB_SUB)  > 0) {
       combo[which(combo$CYTO_T &
                     combo$effector &
-                    combo$CD28M_CD27M),]$POP_NAMES_SUB_SUB = "E"
+                    combo$CD28M_CD27M), ]$POP_NAMES_SUB_SUB = "E"
     }
     
     if (length(combo[which(combo$CYTO_T &
                            combo$effector &
-                           combo$CD28M_CD27P),]$POP_NAMES_SUB_SUB)  > 0) {
+                           combo$CD28M_CD27P), ]$POP_NAMES_SUB_SUB)  > 0) {
       combo[which(combo$CYTO_T &
                     combo$effector &
-                    combo$CD28M_CD27P),]$POP_NAMES_SUB_SUB = "pE2"
+                    combo$CD28M_CD27P), ]$POP_NAMES_SUB_SUB = "pE2"
     }
     
     if (length(combo[which(combo$CYTO_T &
                            combo$effector &
-                           combo$CD28P_CD27P),]$POP_NAMES_SUB_SUB)  > 0) {
+                           combo$CD28P_CD27P), ]$POP_NAMES_SUB_SUB)  > 0) {
       combo[which(combo$CYTO_T &
                     combo$effector &
-                    combo$CD28P_CD27P),]$POP_NAMES_SUB_SUB = "pE1"
+                    combo$CD28P_CD27P), ]$POP_NAMES_SUB_SUB = "pE1"
     }
     
     if (length(combo[which(combo$CYTO_T &
                            combo$effector &
-                           combo$CD28P_CD27M),]$POP_NAMES_SUB_SUB)  > 0) {
+                           combo$CD28P_CD27M), ]$POP_NAMES_SUB_SUB)  > 0) {
       combo[which(combo$CYTO_T &
                     combo$effector &
-                    combo$CD28P_CD27M),]$POP_NAMES_SUB_SUB = "CD28P_27M"
+                    combo$CD28P_CD27M), ]$POP_NAMES_SUB_SUB = "CD28P_27M"
       
     }
     
     if (length(combo[which(combo$CYTO_T &
                            combo$effector.memory &
-                           combo$CD28M_CD27M),]$POP_NAMES_SUB_SUB)  > 0) {
+                           combo$CD28M_CD27M), ]$POP_NAMES_SUB_SUB)  > 0) {
       combo[which(combo$CYTO_T &
                     combo$effector.memory &
-                    combo$CD28M_CD27M),]$POP_NAMES_SUB_SUB = "EM3"
+                    combo$CD28M_CD27M), ]$POP_NAMES_SUB_SUB = "EM3"
     }
     
     if (length(combo[which(combo$CYTO_T &
                            combo$effector.memory &
-                           combo$CD28M_CD27P),]$POP_NAMES_SUB_SUB)  > 0) {
+                           combo$CD28M_CD27P), ]$POP_NAMES_SUB_SUB)  > 0) {
       combo[which(combo$CYTO_T &
                     combo$effector.memory &
-                    combo$CD28M_CD27P),]$POP_NAMES_SUB_SUB = "EM2"
+                    combo$CD28M_CD27P), ]$POP_NAMES_SUB_SUB = "EM2"
     }
     
     if (length(combo[which(combo$CYTO_T &
                            combo$effector.memory &
-                           combo$CD28P_CD27P),]$POP_NAMES_SUB_SUB)  > 0) {
+                           combo$CD28P_CD27P), ]$POP_NAMES_SUB_SUB)  > 0) {
       combo[which(combo$CYTO_T &
                     combo$effector.memory &
-                    combo$CD28P_CD27P),]$POP_NAMES_SUB_SUB = "EM1"
+                    combo$CD28P_CD27P), ]$POP_NAMES_SUB_SUB = "EM1"
     }
     
     if (length(combo[which(combo$CYTO_T &
                            combo$effector.memory &
-                           combo$CD28P_CD27M),]$POP_NAMES_SUB_SUB)  > 0) {
+                           combo$CD28P_CD27M), ]$POP_NAMES_SUB_SUB)  > 0) {
       combo[which(combo$CYTO_T &
                     combo$effector.memory &
-                    combo$CD28P_CD27M),]$POP_NAMES_SUB_SUB = "EM4"
+                    combo$CD28P_CD27M), ]$POP_NAMES_SUB_SUB = "EM4"
       
     }
     
     
-    combo = combo[which(!is.na(combo$POP_NAMES_SUB)),]
+    combo = combo[which(!is.na(combo$POP_NAMES_SUB)), ]
     
     
     subInterest = c("POP_NAMES_SUB", "POP_NAMES_SUB_SUB")
@@ -202,15 +203,30 @@ intclusts = list.files(opt$inputDirectory,
                        pattern = ".IntMatrix.txt.gz$")
 
 for (file in intclusts) {
-  knownPopulationFile = gsub("_subFirst_TRUE_normalize_FALSE.IntMatrix.txt.gz",
+  print(file)
+  knownPopulationFile = gsub(".IntMatrix.txt.gz",
                              ".boolMatrix.txt.gz",
                              file)
   
-  if (file.exists(knownPopulationFile)) {
+  fullOCPopFile = gsub(".IntMatrix.txt.gz",
+                       ".OC.POP.matrix.txt.gz",
+                       file)
+
+  if (file.exists(knownPopulationFile) & file.exists(fullOCPopFile)) {
+    print(knownPopulationFile)
+    print(fullOCPopFile)
+    
     print(paste0("summarizing clusters from ", file, "to ", outDir))
     knownPopulations = read.delim(knownPopulationFile ,
                                   stringsAsFactors = FALSE,
                                   header = TRUE)
+    print(paste0("Pop N ", length(knownPopulations[,1]), "in  ", knownPopulationFile))
+    
+    fullOCPopulations = read.delim(fullOCPopFile ,
+                                   stringsAsFactors = FALSE,
+                                   header = TRUE)
+    print(paste0("Pop N ", length(fullOCPopulations[,1]), "in  ", fullOCPopFile))
+    
     phenoGraphClusters = read.delim(file ,
                                     stringsAsFactors = FALSE,
                                     header = TRUE)
