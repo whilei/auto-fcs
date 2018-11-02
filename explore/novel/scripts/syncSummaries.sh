f01e991a7960dd4e70e18cf3b2569cbc539e79cd
jlanej <jlanej@gmail.com>
2018-11-02 17:08:31 -0500

    convert controlCV to rscript

--- explore/novel/scripts/syncSummaries.sh
+++ explore/novel/scripts/syncSummaries.sh
@@ -7,12 +7,12 @@ Rscript /Users/Kitty/git/auto-fcs/explore/novel/detect/SummaryClusters.R
 
 Rscript /Users/Kitty/git/auto-fcs/explore/novel/metaClusters2/prepare.R
 
-Rscript /Users/Kitty/git/auto-fcs/explore/novel/detect/renderControls.R
+Rscript /Users/Kitty/git/auto-fcs/explore/novel/detect/controlCV.R
 
 cd /Users/Kitty/git/auto-fcs/explore/novel/
 
 zip -r /Users/Kitty/git/auto-fcs/explore/novel/build/metaClusters.build.zip metaClusters2/*
 
-# git save
+git save
 
-https://github.com/PankratzLab/auto-fcs/blob/ecac87c6f3886485840e3ea8085264e5e5cf6a29/explore/novel/build/metaClusters.build.zip
+# https://github.com/PankratzLab/auto-fcs/blob/ecac87c6f3886485840e3ea8085264e5e5cf6a29/explore/novel/build/metaClusters.build.zip
