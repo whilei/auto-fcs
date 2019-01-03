d09743adac82c13303ee934daf4fc649e9e9a5a4
jlanej <jlanej@gmail.com>
2019-01-03 09:16:27 -0600

    :floppy_disk:

--- explore/novel/scripts/runSummarizeAll.sh
+++ explore/novel/scripts/runSummarizeAll.sh
@@ -4,7 +4,7 @@
 #PBS -e $PBS_JOBNAME.$PBS_JOBID.e
 #PBS -o $PBS_JOBNAME.$PBS_JOBID.o
 #PBS -m ae
-#PBS -l mem=124gb,walltime=44:00:00,nodes=1:ppn=24
+#PBS -l mem=62gb,walltime=24:00:00,nodes=1:ppn=24
 
 
 profile.pl -o detectNovelSubs.profile &
